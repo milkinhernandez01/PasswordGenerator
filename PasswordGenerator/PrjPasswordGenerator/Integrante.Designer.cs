1e85af93e94bcf3e85e5725c6dc73eba65e5a783
Milkin <milkinhernandez@gmail.com>
2018-04-16 22:39:24 -0400

    update

--- PasswordGenerator/PrjPasswordGenerator/Integrante.Designer.cs
+++ PasswordGenerator/PrjPasswordGenerator/Integrante.Designer.cs
@@ -37,8 +37,6 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -130,33 +128,12 @@
             this.label7.TabIndex = 7;
             this.label7.Text = "Harvis Batista";
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(310, 190);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(77, 13);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "15-EISN-1-058";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(291, 169);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(107, 21);
-            this.label9.TabIndex = 9;
-            this.label9.Text = "Bendjy Jean";
-            // 
             // Integrante
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(484, 276);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label9);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label4);
@@ -186,7 +163,5 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
     }
 }
